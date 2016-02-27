844b84e37662fc485511ce7dfe59d5c9a4b26470
smcdougall <smcdougall.mot@gmail.com>
2016-02-27 16:00:03 -0500

    Added TwigGetUrl Plugin

---
+++ plugins/TwigGetUrl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * TwigGetUrl - A small plugin to forward Query String data to Twig.
+ *
+ * NotePaper v1.5
+ * http://development.sjmcdougall.com/pico-themes/NotePaper
+ * http://github.com/smcdougall/NotePaper
+ *
+ * @author  Simon McDougall
+ * @link    http://development.sjmcdougall.com/pico-themes/NotePaper
+ * @license https://opensource.org/licenses/GPL-3.0
+ * @version 1.0
+ */
+final class TwigGetUrl extends AbstractPicoPlugin
+{
+  protected $enabled = true;
+
+  protected $dependsOn = array();
+
+  public function onPageRendering(Twig_Environment &$twig, array &$twigVariables, &$templateName)
+  {
+
+      $twigVariables['TwigGetUrl'] = $_GET;
+      $twigVariables['TwigGetUrlEnabled'] = true;
+  }
+}
